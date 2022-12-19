8a90de61d5f64e3e928021dfeedd5e9ef8008611
golosgit <107067096+golosgit@users.noreply.github.com>
2022-12-19 17:33:29 +0100

    Loading component added

---
+++ src/common/Loading/index.js
@@ -0,0 +1,13 @@
+import { Header } from "../Header";
+import { Wrapper, StyledIcon } from "./styled";
+
+export const Loading = () => {
+  return (
+    <>
+      <Header>Search results for "Mulan"</Header>
+      <Wrapper>
+        <StyledIcon />
+      </Wrapper>
+    </>
+  );
+};
---
+++ src/common/Loading/styled.js
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+import { ReactComponent as Icon } from "../images/loading.svg";
+
+export const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-top: 120px;
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
+    margin-top: 24px;
+  }
+`;
+
+export const StyledIcon = styled(Icon)`
+  width: 90px;
+  height: 90px;
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
+    width: 35px;
+    height: 35px;
+  }
+`;
