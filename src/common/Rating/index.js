adaaf56d7af8c5f0efbe97d3ade6f961d56505c3
golosgit <107067096+golosgit@users.noreply.github.com>
2022-12-28 09:58:36 +0100

    Rating component added

---
+++ src/common/Rating/index.js
@@ -0,0 +1,12 @@
+import { Container, StyledIcon, Evaluation } from "./styled";
+import { OptionalText } from "../OptionalText";
+
+export const Rating = () => {
+  return (
+    <Container>
+      <StyledIcon />
+      <Evaluation>7.8</Evaluation>
+      <OptionalText popular>35 votes</OptionalText>
+    </Container>
+  );
+};
---
+++ src/common/Rating/styled.js
@@ -0,0 +1,35 @@
+import styled from "styled-components";
+import { ReactComponent as Star } from "./star.svg";
+
+export const Container = styled.div`
+  margin-top: 12px;
+  display: flex;
+  align-items: flex-end;
+  gap: 12px;
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobileVerticalMax}px) {
+    gap: 8px;
+  }
+`;
+
+export const StyledIcon = styled(Star)`
+  width: 24px;
+  height: auto;
+  transform: translateY(-2px);
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobileVerticalMax}px) {
+    width: 16px;
+  }
+`;
+
+export const Evaluation = styled.p`
+  font-weight: 600;
+  font-size: 16px;
+  line-height: 1.5;
+  margin: 0;
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobileVerticalMax}px) {
+    font-size: 13px;
+    line-height: 1.3;
+  }
+`;
