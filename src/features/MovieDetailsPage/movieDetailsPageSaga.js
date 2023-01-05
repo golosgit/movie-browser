60499d04ae0cd5adfa5bdbeb1b022896660f6268
golosgit <107067096+golosgit@users.noreply.github.com>
2023-01-05 15:50:02 +0100

    movieDetails saga updated

--- src/features/MovieDetailsPage/movieDetailsPageSaga.js
+++ src/features/MovieDetailsPage/movieDetailsPageSaga.js
@@ -1,6 +1,6 @@
 import { put, takeLatest, delay, call, select, all } from "redux-saga/effects";
 import { fetchMovieDetails, fetchMovieDetailsSuccess, fetchError, selectMovieId } from "./movieDetailsPageSlice";
-import { baseUrl, movieDetails, credits, apiKey } from "../api";
+import { baseUrl, movieDetails, movieDetailsCredits, apiKey } from "../api";
 import { getData } from "../getData";
 
 function* fetchMovieDetailsHandler() {
@@ -10,7 +10,7 @@ function* fetchMovieDetailsHandler() {
     yield delay(500);
     const results = yield all({
       movieDetails: call(getData, `${baseUrl}${movieDetails}${id}${apiKey}`),
-      credits: call(getData, `${baseUrl}${movieDetails}${id}${credits}${apiKey}`),
+      credits: call(getData, `${baseUrl}${movieDetails}${id}${movieDetailsCredits}${apiKey}`),
     });
     yield put(fetchMovieDetailsSuccess(results));
   } catch (error) {
