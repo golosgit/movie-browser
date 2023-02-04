b4320720834dded289e5c5a28b30d87d8ae32c7a
golosgit <golosgit@gmail.com>
2023-02-04 14:14:46 +0100

    responsive placeholder added

--- src/common/Navigation/Search/index.js
+++ src/common/Navigation/Search/index.js
@@ -1,10 +1,14 @@
+import { useLocation } from "react-router-dom";
 import { SearchWrapper, StyledInput, SearchIcon } from "./styled";
 
 export const Search = () => {
+  const location = useLocation();
+  const searchText = location.pathname.includes("/movies") ? "movies" : "people";
+
   return (
     <SearchWrapper>
       <SearchIcon />
-      <StyledInput placeholder="Search for movies..."/>
+      <StyledInput placeholder={`Search for ${searchText}...`}/>
     </SearchWrapper>
   );
 };
