c335cfb8a6aa1e047c37cda1b675cf4fd0f36b16
golosgit <107067096+golosgit@users.noreply.github.com>
2023-02-17 16:32:46 +0100

    working serach on PersonDetailsPage

--- src/features/PersonDetailsPage/index.js
+++ src/features/PersonDetailsPage/index.js
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { useParams } from "react-router-dom";
+import { useParams, useSearchParams, useNavigate } from "react-router-dom";
 import { Navigation } from "../../common/Navigation";
 import { Content } from "../../common/Content";
 import { MainWrapper } from "../../common/MainWrapper";
@@ -8,24 +8,33 @@ import { Details } from "../../common/Details";
 import { MovieList } from "../../common/MovieList";
 import { fetchPersonDetails, selectPersonDetails, selectCast, selectCrew, selectStatus } from "./personDetailsPageSlice";
 import { selectGenres, fetchGenres } from "../Genres/genresSlice";
-
+import { searchParamName } from "../../urlParams";
+import { toPeopleList } from "../../routes";
 
 export const PersonDetailsPage = () => {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
   const personDetails = useSelector(selectPersonDetails);
   const cast = useSelector(selectCast);
   const crew = useSelector(selectCrew);
   const status = useSelector(selectStatus);
   const genres = useSelector(selectGenres);
+  const [params] = useSearchParams();
   const { id } = useParams();
 
-  useEffect(() => {
-    dispatch(fetchPersonDetails(id));
+  const searchParam = params.get(searchParamName) || "";
 
-    if (!genres) {
-      dispatch(fetchGenres());
-    }
-  }, [dispatch, id, genres]);
+  useEffect(() => {
+    if (!searchParam) {
+      dispatch(fetchPersonDetails(id));
+
+      if (!genres) {
+        dispatch(fetchGenres());
+      }
+    } else {
+      navigate(`${toPeopleList}?${searchParamName}=${searchParam}`);
+    }   
+  }, [dispatch, id, genres, searchParam, navigate]);
 
   return (
     <>
