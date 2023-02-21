185c90a699151a1ac31bc0d277cf7fe7b622c5af
golosgit <107067096+golosgit@users.noreply.github.com>
2023-02-21 20:27:30 +0100

    search input updating simultaneously with url

--- src/common/Navigation/Search/index.js
+++ src/common/Navigation/Search/index.js
@@ -1,27 +1,36 @@
-import { useLocation } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useLocation, useSearchParams } from "react-router-dom";
 import { SearchWrapper, StyledInput, SearchIcon } from "./styled";
-import { useUrlParams } from "../../../urlParams";
+import { searchParamName, useUrlParams } from "../../../urlParams";
 
 export const Search = () => {
   const location = useLocation();
-
+  const [params] = useSearchParams();
+  const [query, setQuery] = useState("");
   const urlParams = useUrlParams();
   const updateUrlSearchParam = urlParams[1];
-
+  
   const searchText = location.pathname.includes("/movies") ? "movies" : "people";
+  const searchParam = params.get(searchParamName) || "";
 
-  const setQuery = (event) => {
-    setTimeout(() => {
+  const setUrlQuery = (event) => {
+    setQuery(event.target.value);
+    setTimeout(() => { 
       updateUrlSearchParam(event.target.value.trim());
     }, 1000);
   };
+
+  useEffect(() => {
+    setQuery(searchParam);  
+  }, [searchParam]);
 
   return (
     <SearchWrapper>
       <SearchIcon />
       <StyledInput 
+        value={query}
         placeholder={`Search for ${searchText}...`} 
-        onChange={setQuery} 
+        onChange={setUrlQuery} 
       />
     </SearchWrapper>
   );
