98c27629a042dcf37ff32e051862b00a401264da
golosgit <107067096+golosgit@users.noreply.github.com>
2023-05-02 14:15:06 +0200

    deleted unnecessary movie list redux saga code

--- src/core/saga.js
+++ src/core/saga.js
@@ -1,12 +1,10 @@
 import { all } from "redux-saga/effects";
 import { watchFetchMovieDetailsHandler } from "../features/MovieDetailsPage/movieDetailsPageSaga";
-import { watchFetchMovieListHandler } from "../features/MovieListPage/movieListPageSaga";
 import { watchFetchPeopleListHandler } from "../features/PeopleListPage/peopleListPageSaga";
 import { watchFetchPersonDetailsHandler } from "../features/PersonDetailsPage/personDetailsPageSaga";
 
 export default function* saga() {
   yield all([
-    watchFetchMovieListHandler(),
     watchFetchPeopleListHandler(),
     watchFetchMovieDetailsHandler(),
     watchFetchPersonDetailsHandler(),
--- src/core/store.js
+++ src/core/store.js
@@ -1,7 +1,6 @@
 import createSagaMiddleware from "@redux-saga/core";
 import { configureStore } from "@reduxjs/toolkit";
 import movieDetailsReducer from "../features/MovieDetailsPage/movieDetailsPageSlice";
-import movieListReducer from "../features/MovieListPage/movieListPageSlice";
 import peopleListReducer from "../features/PeopleListPage/peopleListPageSlice";
 import personDetailsReducer from "../features/PersonDetailsPage/personDetailsPageSlice";
 import saga from "./saga";
@@ -10,7 +9,6 @@ const sagaMiddleware = createSagaMiddleware();
 
 const store = configureStore({
   reducer: {
-    movieList: movieListReducer,
     peopleList: peopleListReducer,
     movieDetails: movieDetailsReducer,
     personDetails: personDetailsReducer,
