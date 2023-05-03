e269a9f365f3d5feca0b0e4dcfbddcc740aa73ba
golosgit <107067096+golosgit@users.noreply.github.com>
2023-05-03 15:52:28 +0200

    deleted unnecessary movie details redux saga code

--- src/core/saga.js
+++ src/core/saga.js
@@ -1,10 +1,8 @@
 import { all } from "redux-saga/effects";
-import { watchFetchMovieDetailsHandler } from "../features/MovieDetailsPage/movieDetailsPageSaga";
 import { watchFetchPersonDetailsHandler } from "../features/PersonDetailsPage/personDetailsPageSaga";
 
 export default function* saga() {
   yield all([
-    watchFetchMovieDetailsHandler(),
     watchFetchPersonDetailsHandler(),
   ]);
 }
--- src/core/store.js
+++ src/core/store.js
@@ -1,6 +1,5 @@
 import createSagaMiddleware from "@redux-saga/core";
 import { configureStore } from "@reduxjs/toolkit";
-import movieDetailsReducer from "../features/MovieDetailsPage/movieDetailsPageSlice";
 import personDetailsReducer from "../features/PersonDetailsPage/personDetailsPageSlice";
 import saga from "./saga";
 
@@ -8,7 +7,6 @@ const sagaMiddleware = createSagaMiddleware();
 
 const store = configureStore({
   reducer: {
-    movieDetails: movieDetailsReducer,
     personDetails: personDetailsReducer,
   },
   middleware: [sagaMiddleware],
