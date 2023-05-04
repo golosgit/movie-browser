df3744073d79400af4b02b22801375dbd8f8fbd5
golosgit <107067096+golosgit@users.noreply.github.com>
2023-05-04 19:53:44 +0200

    deleted all redux code and removed redux from dependencies

--- src/core/saga.js
+++
@@ -1,8 +0,0 @@
-import { all } from "redux-saga/effects";
-import { watchFetchPersonDetailsHandler } from "../features/PersonDetailsPage/personDetailsPageSaga";
-
-export default function* saga() {
-  yield all([
-    watchFetchPersonDetailsHandler(),
-  ]);
-}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Provider } from "react-redux";
 import { RouterProvider } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
@@ -8,7 +7,6 @@ import { ThemeProvider } from "styled-components";
 import App from "./core/App";
 import { GlobalStyle } from "./core/GlobalStyle";
 import { router } from "./core/routes";
-import store from "./core/store";
 import { theme } from "./core/theme";
 import reportWebVitals from "./reportWebVitals";
 
@@ -19,14 +17,12 @@ root.render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <ReactQueryDevtools />
-      <Provider store={store}>
-        <ThemeProvider theme={theme}>
-          <GlobalStyle />
-          <RouterProvider router={router}>
-            <App />
-          </RouterProvider>
-        </ThemeProvider>
-      </Provider>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <RouterProvider router={router}>
+          <App />
+        </RouterProvider>
+      </ThemeProvider>
     </QueryClientProvider>
   </React.StrictMode>
 );
