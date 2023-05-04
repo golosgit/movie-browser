d123a01fd5985b28f88a64b6edf29019331adeb3
golosgit <107067096+golosgit@users.noreply.github.com>
2023-05-04 19:21:40 +0200

    implemented person details fetching using tanstack query

--- src/features/PersonDetailsPage/index.js
+++ src/features/PersonDetailsPage/index.js
@@ -1,7 +1,6 @@
 import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
 import { useParams, useSearchParams, useNavigate } from "react-router-dom";
-import { useQuery } from "@tanstack/react-query";
+import { useQueries } from "@tanstack/react-query";
 import { Content } from "../../common/Content";
 import { Details } from "../../common/Details";
 import { MainWrapper } from "../../common/MainWrapper";
@@ -9,63 +8,64 @@ import { MovieList } from "../../common/MovieList";
 import { Navigation } from "../../common/Navigation";
 import { toPeopleList } from "../../core/routes";
 import { searchParamName } from "../../core/urlParams";
-import { baseUrl, genres, apiKey } from "../api";
+import { baseUrl, genres, apiKey, personDetails, personDetailsCredits } from "../api";
 import { fetchData } from "../fetchData";
-import { fetchPersonDetails, selectPersonDetails, selectCast, selectCrew, selectStatus } from "./personDetailsPageSlice";
+
+const createUrl = (...elements) => elements.join("");
 
 export const PersonDetailsPage = () => {
-  const dispatch = useDispatch();
   const navigate = useNavigate();
-  const personDetails = useSelector(selectPersonDetails);
-  const cast = useSelector(selectCast);
-  const crew = useSelector(selectCrew);
-  const status = useSelector(selectStatus);
   const [params] = useSearchParams();
   const { id } = useParams();
 
-  const { data } = useQuery(
-    ["genres"], 
-    () => fetchData(`${baseUrl}${genres}${apiKey}`), 
-    {
-      staleTime: 1000 * 60 * 10,
-      cacheTime: 1000 * 60 * 15,
-    }
-  );
-
   const searchParam = params.get(searchParamName) || "";
 
+  const [genresQuery, personDetailsQuery, personDetailsCreditsQuery] = useQueries({
+    queries: [
+      {
+        queryKey: ["genres"],
+        queryFn: () => fetchData(createUrl(baseUrl, genres, apiKey)),
+        staleTime: 1000 * 60 * 10,
+        cacheTime: 1000 * 60 * 15,
+      },
+      {
+        queryKey: ["personDetails"],
+        queryFn: () => fetchData(createUrl(baseUrl, personDetails, id, apiKey)),
+      },
+      {
+        queryKey: ["personDetailsCredits"],
+        queryFn: () => fetchData(createUrl(baseUrl, personDetails, id, personDetailsCredits, apiKey)),
+      },
+    ],
+  });
+
   useEffect(() => {
-    if (!searchParam) {
-      dispatch(fetchPersonDetails(id));
-    } else {
+    if (searchParam) {
       navigate(`${toPeopleList}?${searchParamName}=${searchParam}`);
     }
-  }, [dispatch, id, searchParam, navigate]);
+  }, [searchParam, navigate]);
 
   return (
     <>
       <Navigation />
-      <Content status={status}>
+      <Content status={personDetailsQuery.fetchStatus === "fetching" ? "loading" : personDetailsQuery.status}>
         <MainWrapper person>
-          <Details 
-            personDetails={personDetails} 
-            person="true" 
+          <Details personDetails={personDetailsQuery.data} person="true" />
+          <MovieList
+            title={`Movies - cast${` (${personDetailsCreditsQuery.data?.cast.length})` || ``}`}
+            hideMaxVotes="true"
+            movieList={personDetailsCreditsQuery.data?.cast}
+            credits="true"
+            listView="true"
+            genres={genresQuery.data?.genres}
           />
-          <MovieList 
-            title={`Movies - cast${` (${cast?.length})` || ``}`} 
-            hideMaxVotes="true" 
-            movieList={cast} 
-            credits="true" 
-            listView="true" 
-            genres={data.genres} 
-          />
-          <MovieList 
-            title={`Movies - crew${` (${crew?.length})` || ``}`} 
-            hideMaxVotes="true" 
-            movieList={crew} 
-            credits="true" 
-            listView="true" 
-            genres={data.genres} 
+          <MovieList
+            title={`Movies - crew${` (${personDetailsCreditsQuery.data?.crew.length})` || ``}`}
+            hideMaxVotes="true"
+            movieList={personDetailsCreditsQuery.data?.crew}
+            credits="true"
+            listView="true"
+            genres={genresQuery.data?.genres}
           />
         </MainWrapper>
       </Content>
